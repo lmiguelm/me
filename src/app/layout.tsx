099ad574b75f8532fab3ad7ee5fdc772f79eb249
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-09-12 14:33:35 -0300

    feat: add and configure shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import "./globals.css";
+import { ThemeProvider } from "@/contexts/theme-provider";
+
+import "../global.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
