30c347e48b043ff3e323e6f4c8196b755b31cd80
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-09-15 23:15:55 -0300

    feat: add discord link

--- src/app/(home)/sections/contact-section.tsx
+++ src/app/(home)/sections/contact-section.tsx
@@ -1,10 +1,8 @@
-import { Mail } from "lucide-react";
-
 import { Clipboard } from "@/components/clipboard";
 import { LinearBackground } from "@/components/linear-background";
-import { Link } from "@/components/link";
 import { Subtitle } from "@/components/subtitle";
-import { Tooltip } from "@/components/tooltip";
+import { DiscordLogoIcon } from "@radix-ui/react-icons";
+import { Mail } from "lucide-react";
 
 export function ContactSection() {
   return (
@@ -13,16 +11,23 @@ export function ContactSection() {
 
       <p className="leading-relaxed text-justify text-sm">
         Espero que tenha gostado do meu portfólio. Caso queria entrar em
-        contato, além das minhas redes sociais, segue meu e-mail:
+        contato, além das minhas redes sociais, segue meu E-mail e Discord:
       </p>
 
-      <div className="flex items-center gap-3">
-        <Mail className="h-4 w-4 text-muted-foreground max-sm:hidden" />
-        <Tooltip message="Clique para copiar">
-          <Clipboard textToBeCopied="luismiguelfernandes.marcelo@gmail.com">
-            <Link title="luismiguelfernandes.marcelo@gmail.com" />
-          </Clipboard>
-        </Tooltip>
+      <div className="flex flex-col items-start">
+        <div className="flex items-center gap-2">
+          <Mail className="h-4 w-4" />
+
+          <Clipboard
+            position="right"
+            textToBeCopied="luismiguelfernandes.marcelo@gmail.com"
+          />
+        </div>
+
+        <div className="flex items-center gap-2">
+          <DiscordLogoIcon className="h-4 w-4" />
+          <Clipboard position="right" textToBeCopied="lmiguelm" />
+        </div>
       </div>
     </LinearBackground>
   );
--- src/components/clipboard.tsx
+++ src/components/clipboard.tsx
@@ -1,22 +1,40 @@
 "use client";
 
-import { Children, cloneElement, isValidElement } from "react";
+import { CheckCircle2, Copy } from "lucide-react";
+import { useState } from "react";
+
+import { twMerge } from "tailwind-merge";
+import { Button } from "./ui/button";
 
 type Props = {
-  children: React.ReactNode;
   textToBeCopied: string;
+  position?: "left" | "right";
 };
 
-export function Clipboard(props: Props) {
+export function Clipboard({ textToBeCopied, position = "left" }: Props) {
+  const [hasCopied, setHasCopied] = useState<boolean>(false);
+
   function handleCopy() {
-    navigator.clipboard.writeText(props.textToBeCopied);
+    navigator.clipboard.writeText(textToBeCopied);
+    setHasCopied(true);
   }
 
-  return Children.map(props.children, (child) => {
-    if (!isValidElement(child)) return null;
+  return (
+    <Button
+      variant="link"
+      onClick={handleCopy}
+      className={twMerge(
+        "flex items-center gap-3 cursor-pointer p-0",
+        position === "right" ? "flex-row-reverse" : "flex-row"
+      )}
+    >
+      {hasCopied ? (
+        <CheckCircle2 className="h-4 w-4 text-green-500 max-sm:hidden" />
+      ) : (
+        <Copy className="h-4 w-4 text-muted-foreground max-sm:hidden" />
+      )}
 
-    return cloneElement(child, { onClick: handleCopy } as {
-      onClick: () => void;
-    });
-  });
+      <span className="text-muted-foreground">{textToBeCopied}</span>
+    </Button>
+  );
 }
