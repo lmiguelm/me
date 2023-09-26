30c5017eeacaf32e4fdce27a4814d03727007a7c
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-09-25 23:20:22 -0300

    feat: add tags in float button

--- src/components/float-actions.tsx
+++ src/components/float-actions.tsx
@@ -3,20 +3,22 @@
 import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 import { useRouter } from "next/navigation";
 
-import { ArrowUp } from "lucide-react";
+import { ArrowLeft } from "lucide-react";
 
 import { useState } from "react";
+import { Link } from "./link";
 import { Button } from "./ui/button";
 
 export function FloatActions() {
   const { scrollYProgress } = useScroll();
   const router = useRouter();
 
-  const [hasScroll, setHasScroll] = useState<number>(0);
+  const [hasScroll, setHasScroll] = useState<boolean>(false);
 
-  useMotionValueEvent(scrollYProgress, "change", (value) =>
-    setHasScroll(value)
-  );
+  useMotionValueEvent(scrollYProgress, "change", (value) => {
+    setHasScroll(value !== 0 && value !== 1);
+  });
+
   function handleClick() {
     if (window.scrollY === 0) {
       return router.back();
@@ -29,20 +31,20 @@ export function FloatActions() {
   }
 
   return (
-    <div className="fixed top-[150px] right-[15%] space-y-6 max-lg:hidden">
+    <div className="flex flex-col fixed top-[150px] right-[15%] space-y-6 max-lg:hidden">
       <Button
         variant="secondary"
-        className="flex items-center justify-center p-2"
+        className="flex items-center justify-center p-2 w-9 h-9"
         onClick={handleClick}
       >
-        <motion.div animate={{ rotate: hasScroll ? 0 : -90 }}>
-          <ArrowUp className="w-4 h-4" />
+        <motion.div animate={{ rotate: hasScroll ? 90 : 0 }}>
+          <ArrowLeft className="w-4 h-4" />
         </motion.div>
       </Button>
 
       <Button
         variant="outline"
-        className="flex items-center justify-center p-2"
+        className="flex items-center justify-center p-2 w-9 h-9"
         disabled
       >
         <svg className="w-4 h-8" viewBox="0 0 100 100">
@@ -68,6 +70,12 @@ export function FloatActions() {
           />
         </svg>
       </Button>
+
+      <Link href="/projects/tag/mobile" title="Mobile" />
+
+      <Link href="/projects/tag/front-end" title="Front-end" />
+
+      <Link href="/projects/tag/back-end" title="Back-end" />
     </div>
   );
 }
