e65b361a9bf6e1661927389a44ed19b000456e33
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-09-29 18:48:11 -0300

    feat: back button and animation

--- src/components/float-action/float-action-arrow.tsx
+++ src/components/float-action/float-action-arrow.tsx
@@ -1,10 +1,31 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+import { useScroll } from "framer-motion";
 import { ArrowUp } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { MotionDiv } from "../motion-div";
 import { Button } from "../ui/button";
 
 export function FloatActionArrow() {
+  const { scrollYProgress } = useScroll();
+  const router = useRouter();
+
+  const [scroll, setScroll] = useState(0);
+  const hasScroll = scroll > 0;
+
+  useEffect(() => {
+    scrollYProgress.on("change", setScroll);
+    return () => scrollYProgress.clearListeners();
+  }, [scrollYProgress]);
+
   function handleClick() {
+    if (!hasScroll) {
+      return router.back();
+    }
+
     return window.scrollTo({
       behavior: "smooth",
       top: 0,
@@ -17,7 +38,9 @@ export function FloatActionArrow() {
       className="flex items-center justify-center p-2 w-9 h-9"
       onClick={handleClick}
     >
-      <ArrowUp className="w-4 h-4" />
+      <MotionDiv initial={{}} animate={{ rotate: !hasScroll ? "-90deg" : 0 }}>
+        <ArrowUp className="w-4 h-4" />
+      </MotionDiv>
     </Button>
   );
 }
--- src/components/float-action/float-action-root.tsx
+++ src/components/float-action/float-action-root.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MotionDiv } from "../motion-div";
 
 type Props = {
   children: React.ReactNode;
@@ -6,8 +7,11 @@ type Props = {
 
 export function FloatActionRoot({ children }: Props) {
   return (
-    <div className="flex flex-row justify-center items-center space-x-6 fixed bottom-0 bg-highlight/20 backdrop-blur-3xl w-full z-20 p-4">
+    <MotionDiv
+      initial={{ opacity: 0 }}
+      className="flex flex-row justify-center items-center space-x-6 fixed bottom-0 bg-highlight/20 backdrop-blur-3xl w-full z-20 p-4"
+    >
       {children}
-    </div>
+    </MotionDiv>
   );
 }
