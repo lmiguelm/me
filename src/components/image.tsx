29041c2ab8a029b6ddee09d1eafb4e0950f7d56a
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-09-29 00:53:50 -0300

    fix: remove plaiceholder and add local shimmer

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,29 +1,27 @@
 import { PrismicNextImage, PrismicNextImageProps } from "@prismicio/next";
-import { getPlaiceholder } from "plaiceholder";
 
 export async function Image(props: PrismicNextImageProps) {
-  async function getBlurImg(url?: string | null) {
-    if (!url) {
-      return "/public/placeholder.svg";
-    }
+  const shimmer = (w: number, h: number) => `
+    <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <defs>
+        <linearGradient id="g">
+          <stop stop-color="#555" offset="20%" />
+          <stop stop-color="#333" offset="50%" />
+          <stop stop-color="#555" offset="70%" />
+        </linearGradient>
+      </defs>
+      <rect width="${w}" height="${h}" fill="#555" />
+      <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+      <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+    </svg>
+  `;
 
-    try {
-      const response = await fetch(url, { cache: "force-cache" });
-
-      const buffer = Buffer.from(await response.arrayBuffer());
-      const { base64 } = await getPlaiceholder(buffer);
-
-      return base64;
-    } catch {
-      return "";
-    }
-  }
+  const toBase64 = (str: string) => Buffer.from(str).toString("base64");
 
   return (
     <PrismicNextImage
       {...props}
-      placeholder="blur"
-      blurDataURL={await getBlurImg(props.field?.url)}
+      placeholder={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
       alt={props.alt ?? ""}
     />
   );
