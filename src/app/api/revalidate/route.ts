73d93ab6f08a4a43dd6f3d159d48e0bec9529b78
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-09-29 01:23:20 -0300

    refact: security clear cache route

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,7 +1,21 @@
 import { revalidateTag } from "next/cache";
 import { NextResponse } from "next/server";
 
-export async function POST() {
+type WebhookPayload = {
+  secret: string;
+};
+
+export async function POST(request: Request) {
+  const req = await request.json();
+  const { secret } = req as WebhookPayload;
+
+  if (secret !== process.env.PRISMIC_SECRET_WEBHOOK) {
+    return new NextResponse(null, {
+      statusText: "Invalid webhook secret key",
+      status: 400,
+    });
+  }
+
   revalidateTag("prismic");
 
   return NextResponse.json({ revalidated: true, now: Date.now() });
