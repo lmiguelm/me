661687565bd617e0ab07730498c89f3e6f9ebced
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-09-30 18:17:56 -0300

    fix: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR" suppressHydrationWarning suppressContentEditableWarning>
+      <head>
+        <link
+          rel="icon"
+          href="/favicon.ico"
+          sizes="32x32"
+          type="image/x-icon"
+        />
+
+        <link
+          rel="icon"
+          href="/favicon-16x16.png"
+          sizes="16x16"
+          type="image/png"
+        />
+
+        <link
+          rel="icon"
+          href="/favicon-32x32.png"
+          sizes="32x32"
+          type="image/png"
+        />
+      </head>
+
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
