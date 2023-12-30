1cc930ef84d2ce816bdf4fa01e03f4178f54107c
lmiguelm <luismiguelfernandes.marcelo@gmail.com>
2023-12-30 15:36:19 -0300

    fix: build eslint error

--- src/components/sub-header.tsx
+++ src/components/sub-header.tsx
@@ -10,7 +10,7 @@ type Props = {
   links?: { url: string; title: string }[]
 }
 
-export function SubHeader(props: Props) {
+export function SubHeader({ links, message, title }: Props) {
   return (
     <MotionDiv
       initial={{ y: 100, opacity: 0 }}
@@ -22,27 +22,26 @@ export function SubHeader(props: Props) {
         variant="highlight"
         className="rounded flex flex-col items-center space-y-3 p-3 backdrop-blur-3xl"
       >
-        <Title>{props.title}</Title>
+        <Title>{title}</Title>
 
-        <p className="leading-relaxed text-sm text-center">{props.message}</p>
+        <p className="leading-relaxed text-sm text-center">{message}</p>
 
         <div>
-          {!!props.links &&
-            props.links.map((link, index) => (
-              <div className="flex items-center gap-3" key={link.url}>
-                <Link
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href={link.url}
-                  key={link.url}
-                  title={link.title}
-                />
+          {links?.map((link, index) => (
+            <div className="flex items-center gap-3" key={link.url}>
+              <Link
+                target="_blank"
+                rel="noopener noreferrer"
+                href={link.url}
+                key={link.url}
+                title={link.title}
+              />
 
-                {index !== props.links?.length - 1 && (
-                  <Separator orientation="vertical" className="h-3" />
-                )}
-              </div>
-            ))}
+              {index !== links.length - 1 && (
+                <Separator orientation="vertical" className="h-3" />
+              )}
+            </div>
+          ))}
         </div>
       </LinearBackground>
     </MotionDiv>
